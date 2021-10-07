f965dc2a9ecba8cccef2b3302d96f9b301280139
Dawit Gebeta <dawitgebeta@gmail.com>
2021-10-07 11:53:08 -0400

    product viewset django

--- yetal_django/product/views.py
+++ yetal_django/product/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from rest_framework import serializers
 
 from rest_framework.views import APIView
@@ -6,10 +7,23 @@ from rest_framework.response import Response
 from .models import Product, Category
 from .serializers import ProductSerializer
 
+
 class LatestProductList(APIView):
-    def get(self, request, format = None):
+    def get(self, request, format=None):
         products = Product.objects.all()[0:4]
-        serializer = ProductSerializer(products, many = True)
+        serializer = ProductSerializer(products, many=True)
 
         return Response(serializer.data)
-        
+
+
+class ProductDetail(APIView):
+    def get_object(self, category_slug, product_slug):
+        try:
+            return Product.objects.filter(category_slug=category_slug).get(slug=product_slug)
+        except Product.DoesNotExist:
+            raise Http404
+
+    def get(self, request, category_slug, product_slug, format=None):
+        product = self.get_object(category_slug, product_slug)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
