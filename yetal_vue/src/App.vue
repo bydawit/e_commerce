167e9c9d0ade6829a7b71fc363f28da523dd0867
Dawit Gebeta <dawitgebeta@gmail.com>
2021-10-09 19:55:37 -0400

    Category view, ProductBox

--- yetal_vue/src/App.vue
+++ yetal_vue/src/App.vue
@@ -23,8 +23,8 @@
         v-bind:class="{ 'is-active': showMobileMenu }"
       >
         <div class="navbar-end">
-          <router-link to="/Summer" class="navbar-item">Summer</router-link>
-          <router-link to="/Winter" class="navbar-item">Winter</router-link>
+          <router-link to="/summer" class="navbar-item">Summer</router-link>
+          <router-link to="/winter" class="navbar-item">Winter</router-link>
           <div class="navbar-item">
             <div class="buttons">
               <router-link to="/log-in" class="button is-light"
--- yetal_vue/src/views/Home.vue
+++ yetal_vue/src/views/Home.vue
@@ -10,33 +10,19 @@
       <div class="column is-12">
         <h2 class="is-size-2 has-text-centered">Latest Products</h2>
       </div>
-      <div
-        class="column is-3"
+      <ProductBox
         v-for="product in latestProducts"
         v-bind:key="product.id"
-      >
-        <div class="box">
-          <figure class="image mb-4">
-            <img :src="product.get_thumbnail" />
-          </figure>
-          <h3 class="is-size-4">
-            {{ product.name }}
-          </h3>
-          <p class="is-size-6 has-text-grey">${{ product.price }}</p>
-          <router-link
-            v-bind:to="product.get_absolute_url"
-            class="button is-dark mt-4"
-          >
-            View Details
-          </router-link>
-        </div>
-      </div>
+        v-bind:product="product"
+      />
     </div>
   </div>
 </template>
 
 <script>
 import axios from "axios";
+import ProductBox from "@/components/ProductBox";
+
 export default {
   name: "Home",
   data() {
@@ -44,7 +30,9 @@ export default {
       latestProducts: [],
     };
   },
-  components: {},
+  components: {
+    ProductBox,
+  },
   mounted() {
     this.getLatestProducts();
 
@@ -68,11 +56,3 @@ export default {
   },
 };
 </script>
-
-<style scoped>
-.image {
-  margin-top: -1.25rem;
-  margin-left: -1.25rem;
-  margin-right: -1.25rem;
-}
-</style>
